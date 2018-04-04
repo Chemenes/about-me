05bcc5c4a4c6fa7e9ce2f34edf9dbe4821c84721
Jane Smith <example@email.com>
2018-04-03 17:26:09 -0700

    Linked my index to js by using the js script tag.Once completedadded my five personalquestions aboutmyself in the js file.I used variables and prompt for the yes and no questions.Concole log to log that information and an alert to give the user a correct or incorrect answer.

---
+++ js/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+alert('Few people may know some pretty interesting facts about my life. I will list some off and you try and guess if they are true or false.')
+
+var snowFall = prompt('I once got stuck in a snow tunnel and no one could find me for 6 hours?')
+console.log('I once got stuck in a snow tunnel and noone could find me for 6 hours?' + snowFall)
+alert('This is false, although i did get stuck in a snow tunnel it only took about 5 minutes to get me out.')
+
+var skateBoard = prompt('I\'ve been skateboarding for more than half my life and haven\'t broken a bone?')
+console.log('I\'ve been skateboarding for more than half my life and haven\'t broken a bone?' + skateBoard) 
+alert('This is true, my ankles have been twisted so much they can do a 360 but not one broken bone!')
+
+var swim = prompt('I can swim')
+console.log('I can swim' + swim)
+alert('I can swim! Although i cannot stay afloat in deep water.')
+
+var sunAllergy = prompt('My sun allergy is why I moved to Seattle')
+console.log('My sun allergy is why I moved to Seattle' + sunAllergy)
+alert('False! I love the sun, even though Seattle hides it away. I gave no allergies but can\'t stand tomatoes')
+
+var clubTurkey = prompt ('I once bowled a perfect game')
+console.log('I once bowled a perfect game' + clubTurkey)
+alert('Sadly wrong! I did bowl a 290, so close but so far')
+
+alert('Thank you for playing, now you know a little more about me!')
